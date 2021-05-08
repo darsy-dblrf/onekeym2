619cfd16b90f9ff89b5d5b44b1628e70e8590283
Darsy <chenjunda@shuangyulin.cn>
2021-05-08 21:06:52 +0800

    init

--- onekeym2.sh
+++ onekeym2.sh
@@ -16,6 +16,14 @@ printf "
 # Check if user is root
 [ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
 
+
+#input variable
+read -e -p "Please input domain(example: www.magento2.com): " domain
+read -e -p "Please input Database username(example: mg2user): " dbuser
+read -e -p "Please input Database name(example: mg2db): " dbname
+dbpassword=`openssl rand -base64 12`
+
+#start install
 apt update
 apt upgrade -y
 apt install apt-transport-https -y
@@ -24,3 +32,8 @@ add-apt-repository ppa:ondrej/php -y
 curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | bash -s -- --mariadb-server-version=10.4 --skip-maxscale
 apt install mariadb-server  mariadb-client -y
 curl -sSL https://get.docker.com | bash
+
+mysql -e "create database $dbname;"
+mysql -e "CREATE USER '$dbuser'@'localhost' IDENTIFIED BY '$dbpassword';"
+mysql -e "grant all privileges on $dbname.* TO '$dbuser'@'localhost';"
+mysql -e "flush privileges;"
